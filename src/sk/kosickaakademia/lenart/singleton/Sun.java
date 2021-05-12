fed5decccf8d66c7ef3cc3458354da13e37f3719
SamuelLenart <samuel.lenart@kosickaakademia.sk>
2021-05-12 13:27:22 +0200

    add smallestDistance

--- src/sk/kosickaakademia/lenart/singleton/Sun.java
+++ src/sk/kosickaakademia/lenart/singleton/Sun.java
@@ -1,9 +1,8 @@
 package sk.kosickaakademia.lenart.singleton;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Sun {
     private static Sun instance = new Sun();
@@ -26,4 +25,42 @@ public class Sun {
             System.out.println(planet.getName());
         }
     }
+
+    public ArrayList<Planet> findByPattern(){
+        String regex="^.*Mer.*$";
+        Pattern p = Pattern.compile(regex);
+        for (Planet plannet:set) {
+            Matcher m=p.matcher(plannet.getName());
+            if (m.matches()) {
+                System.out.println(plannet.getName());
+
+            }
+        }
+        return (ArrayList<Planet>) set;
+    }
+
+    public void smallestDistance() {
+        int temp, size;
+        int array[] = {1499600000, 227900000, Math.toIntExact(2871000000000l), Math.toIntExact(1434000000000l), 778500000, Math.toIntExact(4495000000000l), 108200000};
+        size = array.length;
+
+        for(int i = 0; i<size; i++ ){
+            for(int j = i+1; j<size; j++){
+                if(array[i]>array[j]){
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        System.out.println("Smallest distance of the planets is: "+array[0]);
+    }
+
+    /*Planet pom = set.get(0);
+        for(Planet t = set)
+            if (pom.getD()>t.getD())
+                pom=t;
+                pom.getName();
+     */
 }
+
